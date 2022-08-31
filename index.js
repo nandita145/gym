00d4622788f3d6768487977565fbeba92e08f0ce
nandita145 <92240425+nandita145@users.noreply.github.com>
2022-08-31 17:22:24 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -5,3 +5,55 @@ function openNav() {
 function closeNav() {
     document.getElementById("mobile__menu").style.width = "0";
 }
+
+// $(document).ready(function () {
+//     $('.radio').click(function () {
+//         document.getElementById('price').innerHTML = $(this).val();
+//     });
+
+// });
+
+function checkRadio(value) {
+    if(value == "1"){
+    // console.log("Choice: ", value);
+        // document.getElementById("one-variable-equations").checked = true;
+        // document.getElementById("multiple-variable-equations").checked = false;
+        document.getElementById("price-checkout").innerHTML = 'Rs. 4900 only';
+    } else if (value == "6"){
+        document.getElementById("price-checkout").innerHTML = 'Rs. 7900 only';
+    }else if (value == "12"){
+        document.getElementById("price-checkout").innerHTML = 'Rs. 9900 only';
+    }
+}
+// const signUpButton = document.getElementById('signUp');
+// const signInButton = document.getElementById('signIn');
+// const container = document.getElementById('container');
+
+// signUpButton.addEventListener('click', () => {
+// 	container.classList.add("right-panel-active");
+// });
+
+// signInButton.addEventListener('click', () => {
+// 	container.classList.remove("right-panel-active");
+// });
+
+
+
+    
+    
+// $('.counter').counterUp({
+//     delay: 10,
+//     time: 2000
+// });
+// $('.counter').addClass('animated fadeInDownBig');
+// $('h3').addClass('animated fadeIn');
+
+
+$('[data-toggle="counter-up"]').counterUp({
+    delay: 10,
+    time: 2000
+});
+
+
+
+

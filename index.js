4f648f8e1b28c0693afd95b9e0339736295fb12b
nandita145 <92240425+nandita145@users.noreply.github.com>
2022-09-01 20:22:10 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -25,6 +25,53 @@ function checkRadio(value) {
         document.getElementById("price-checkout").innerHTML = 'Rs. 9900 only';
     }
 }
+
+
+
+
+
+$('[data-toggle="counter-up"]').counterUp({
+    delay: 10,
+    time: 2000
+});
+
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  // Close the dropdown if the user clicks outside of it
+  window.onclick = function(e) {
+    if (!e.target.matches('.dropbtn')) {
+    var myDropdown = document.getElementById("myDropdown");
+      if (myDropdown.classList.contains('show')) {
+        myDropdown.classList.remove('show');
+      }
+    }
+  }
+
+
+  const form = document.querySelector('form');
+
+  form.addEventListener('submit', function(e){
+      e.preventDefault();
+      
+      const height = parseInt(document.querySelector('#height').value);
+      const weight = parseInt(document.querySelector('#weight').value);
+      const results = document.querySelector('#results');
+      
+      if((height === '') || (height < 0) || (isNaN(height))){
+          //NaN !== NaN
+          results.innerHTML = "Please provide a valid height";
+          
+      } else if (weight === '' || weight < 0 || isNaN(weight)){
+          results.innerHTML = "Please provide a valid weight";
+      } else {
+      //calculate BMI
+      const bmi = (weight / ((height*height)/10000)).toFixed(2);
+      //display the results
+      results.innerHTML = `<span>${bmi}</span>`
+      }  
+  });
 // const signUpButton = document.getElementById('signUp');
 // const signInButton = document.getElementById('signIn');
 // const container = document.getElementById('container');
@@ -49,10 +96,6 @@ function checkRadio(value) {
 // $('h3').addClass('animated fadeIn');
 
 
-$('[data-toggle="counter-up"]').counterUp({
-    delay: 10,
-    time: 2000
-});
 
 
 
